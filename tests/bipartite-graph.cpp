fe60a45255df73ca0fdcaaa87af35cbb9bb15cda
Orif Milod <milodo@amazon.com>
2021-02-01 20:03:56 +0100

    Fix CmakeLists

---
+++ tests/bipartite-graph.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include <iostream>
+
+using namespace std;
+
+TEST(RootFolder, SomethingNew) { EXPECT_EQ(18.0, 18.0); }
+
+int main() { return 0; }
--- tests/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-#include <iostream>
-
-using namespace std;
-
-int main() {
-  ::testing::InitGoogleTest();
-  return RUN_ALL_TESTS();
-}
