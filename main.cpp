965be89cd76d67620a783ed0a8e101dd08ded2c2
orifmilod <orifmilod@gmail.com>
2021-02-01 20:44:20 +0100

    Fix linking libraries

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ using namespace std;
 TEST(RootFolder, SomethingNew) { EXPECT_EQ(18.0, 18.0); }
 
 int main() {
-  ::testing::InitGoogleTest();
+  testing::InitGoogleTest();
   return RUN_ALL_TESTS();
 }
